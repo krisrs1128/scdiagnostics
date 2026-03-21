bce40639f94701017dd9cf026ebf17eccf3b942f
Kris Sankaran <sankaran.kris@gmail.com>
2026-03-21 14:27:54 -0500

    package infrastructure

---
+++ src/scdiagnostics/__init__.py
@@ -0,0 +1,11 @@
+from .dimred import plot_umap, plot_pca
+from .marginal import (
+    compare_boxplot,
+    compare_ecdf,
+    compare_histogram,
+    compare_means,
+    compare_moments,
+    compare_standard_deviation,
+    compare_variances,
+    compare_histogram2
+)
